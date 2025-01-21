071f0649d09acdcdf1ebc6d8ca1f37707970c3e7
TJ31TJ <achrafhas2003@gmail.com>
2025-01-21 23:23:45 +0000

    Add files via upload

---
+++ brick breaking/loading.js
@@ -0,0 +1,32 @@
+
+const BG_IMG = new Image();
+BG_IMG.src = "img/bg.jpg";
+
+const BALL_IMG= new Image();
+BALL_IMG.src="img/bol.png";
+
+const LEVEL_IMG = new Image();
+LEVEL_IMG.src = "img/level.png";
+
+const LIFE_IMG = new Image();
+LIFE_IMG.src = "img/heart.jpeg";
+
+const SCORE_IMG = new Image();
+SCORE_IMG.src = "img/score.png";
+const WALL_HIT = new Audio();
+WALL_HIT.src = "effect/wall.mp3";
+
+const bg_music= new Audio();
+bg_music.src = "effect/bgmusic.mp3";
+
+const LIFE_LOST = new Audio();
+LIFE_LOST.src = "effect/life_lost.mp3";
+
+const PADDLE_HIT = new Audio();
+PADDLE_HIT.src = "effect/paddle_hit.mp3";
+
+const WIN = new Audio();
+WIN.src = "effect/win.mp3";
+const BRICK_HIT = new Audio();
+BRICK_HIT.src = "effect/brick_hit.mp3";
+
